2fa04fa3a94a773a1551ce22fb44e53bb89696a3
thakuraditya77 <adityaadityarana777@gmail.com>
2026-01-31 08:19:12 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+class Demo {
+
+    int number = 10;
+
+    public static void main(String[] args) {
+
+        Demo obj = new Demo();
+
+        System.out.println("Value of number: " + obj.number);
+    }
+}
