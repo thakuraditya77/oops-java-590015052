8d803ec56cbecff667cdeeb7414c398486afdc5d
thakuraditya77 <adityaadityarana777@gmail.com>
2026-02-13 13:09:18 +0530

    Add files via upload

---
+++ Abrastraction.java
@@ -0,0 +1,32 @@
+abstract class Sample {
+
+    int a = 10;
+
+    void f() {
+        System.out.println(a);
+    }
+
+    abstract void f2();
+}
+
+class Child extends Sample {
+
+    void show() {
+        super.f();
+    }
+
+    void f2() {
+        System.out.println("done");
+    }
+}
+
+public class Abrastraction {
+
+    public static void main(String[] args) {
+
+        Child s = new Child();
+        s.show();
+        s.f2();
+
+    }
+}
